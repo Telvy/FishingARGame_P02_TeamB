32030a4f36df0179865b9801b18150123c48ed4a
Adam <Adamqplett@gmail.com>
2022-05-03 03:15:02 -0500

    Ui Main Menu
    Minor changes to main menu UI

--- Assets/_TeamB_P02_AR/Scripts/TestSpawnBobber.cs
+++ Assets/_TeamB_P02_AR/Scripts/TestSpawnBobber.cs
@@ -21,7 +21,6 @@ public class TestSpawnBobber : MonoBehaviour
             {
                 SpawnBobber();
                 ActiveBobbers--;
-                //Pond.instance.SearchForFish();
             }
         }
 
